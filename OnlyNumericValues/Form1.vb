edbd57eb9a42d8b4d84badc3ffb2f33e1384ff55
nomvula <nzshabangu95@gmail.com>
2023-09-08 10:22:46 +0200

    Add project files.

---
+++ OnlyNumericValues/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnNum_Click(sender As Object, e As EventArgs) Handles btnNum.Click
+        Dim num As Integer
+
+
+        If IsNumeric(txtNum.Text) = True Then
+            num = txtNum.Text
+        Else
+            MsgBox("INVALID! ENTER A NUMBER")
+            Exit Sub
+        End If
+        MsgBox(num)
+
+    End Sub
+End Class
